53fdc1d28af717b1301e97d6f7b5761cd5ef839a
Audrey Jiang <ajiang@lcs.on.ca>
2022-11-07 11:37:10 -0500

    Initial Commit

---
+++ HeaderExample/HeaderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderExampleApp.swift
+//  HeaderExample
+//
+//  Created by Audrey Jiang on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct HeaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
